643ecee80f05d720a6e34026120ce7440ac73f8a
Saiabhileshpadigela <saiabhileshpadigela@gmail.com>
2024-05-30 20:52:25 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -6,25 +6,95 @@ const PORT = 5000;
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); 
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
+async function getAuthToken() {
+    try {
+        const res = await fetch('http://20.244.56.144/test/auth', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "companyName": "goMart",
+                "clientID": "62a52540-a418-4cad-9986-9e3a5ba4e940",
+                "clientSecret": "rrDicPLmVYtXAUII",
+                "ownerName": "Abhilesh",
+                "ownerEmail": "saiabhileshpadigela@gmail.com",
+                "rollNo": "21bd1a057a"
+            })
+        });
+
+        const data = await res.json();
+        console.log(data)
+
+        return data.access_token;
+    } catch (error) {
+        console.error('Error fetching authentication token:', error);
+        throw error;
+    }
+}
+
 app.get('/api/products', async (req, res) => {
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzE3MDc5MzI1LCJpYXQiOjE3MTcwNzkwMjUsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjYyYTUyNTQwLWE0MTgtNGNhZC05OTg2LTllM2E1YmE0ZTk0MCIsInN1YiI6InNhaWFiaGlsZXNocGFkaWdlbGFAZ21haWwuY29tIn0sImNvbXBhbnlOYW1lIjoiZ29NYXJ0IiwiY2xpZW50SUQiOiI2MmE1MjU0MC1hNDE4LTRjYWQtOTk4Ni05ZTNhNWJhNGU5NDAiLCJjbGllbnRTZWNyZXQiOiJyckRpY1BMbVZZdFhBVUlJIiwib3duZXJOYW1lIjoiQWJoaWxlc2giLCJvd25lckVtYWlsIjoic2FpYWJoaWxlc2hwYWRpZ2VsYUBnbWFpbC5jb20iLCJyb2xsTm8iOiIyMWJkMWEwNTdhIn0._NMYKV2kCjSJ8xmRPfhApDM3UeMPVY_GSCKBGFkVYto'
 
     try {
-        const response = await fetch('http://20.244.56.144/test/companies/AMZ/categories/Laptop/products?top=10&minPrice=1&maxPrice=18000', {
+        const authToken = await getAuthToken();
+        console.log(authToken);
+
+        const res = await fetch('http://20.244.56.144/test/companies/AMZ/categories/Laptop/products?top=10&req.query.min=1&req.query.max=18000', {
             headers: {
-                'Authorization': `Bearer ${token}`
+                'Authorization': `Bearer ${authToken}`
             }
         });
-        const data = await response.json();
+        const data = await res.json();
         console.log(data)
         res.json(data);
     } catch (error) {
-        console.error('Error fetching products:', error);
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.get("/products", async (req, res) => {
+    const companies = ["AMZ", "FLP", "SNP", "MYN", "AZO"];
+    const categories = ["Phone", "Computer", "TV", "Earphone", "Tablet", "Charger", "Mouse", "Keypad", "Bluetooth", "Pendrive", "Remote", "Speaker", "Headset", "Laptop", "PC"];
+
+    const products = [];
+
+    try {
+        const authToken = await getAuthToken();
+        console.log(authToken);
+
+        for (const company of companies) {
+            for (const category of categories) {
+                const url = `http://20.244.56.144/companies/${company}/categories/${category}/products?top=${req.query.limit || 10}&minPrice=${req.query.min || 1}&maxPrice=${req.query.max || 10000}`;
+                console.log(url);
+
+                const res = await fetch(url, {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': `Bearer ${authToken}`
+                    }
+                });
+
+                const data = await res.json();
+                console.log(JSON.stringify(data));
+
+                data.forEach((product) => {
+                    product.company = company;
+                    product.category = category;
+                    products.push(product);
+                });
+
+            }
+        }
+
+        res.json({ status: 0, products: products });
+    } catch (error) {
+        console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
