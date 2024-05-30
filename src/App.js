0c6d38d681916cd1fe82da9ecf83aceabaf6ec38
Saiabhileshpadigela <saiabhileshpadigela@gmail.com>
2024-05-30 20:16:29 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AllProducts from './components/AllProducts';
+import ProductDetails from './components/ProductDetails';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<AllProducts/>} />
+				<Route path="/product/:productId" element={<ProductDetails/>} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
